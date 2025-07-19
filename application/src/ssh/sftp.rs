6d2cbd6b8f6e957cd528a0fb9d4c8f7ad60d3ff7
Robert Jansen <me@rjns.dev>
2025-07-19 21:35:45 +0200

    winscp moment

--- application/src/ssh/sftp.rs
+++ application/src/ssh/sftp.rs
@@ -1457,8 +1457,8 @@ impl russh_sftp::server::Handler for SftpSession {
                     russh_sftp::protocol::ExtendedReply {
                         id,
                         data: russh_sftp::ser::to_bytes(&LimitsReply {
-                            max_packet_length: 256 * 1024,
-                            max_read_length: 192 * 1024,
+                            max_packet_length: 192 * 1024,
+                            max_read_length: 128 * 1024,
                             max_write_length: 128 * 1024,
                             max_handle_count: HANDLE_LIMIT as u64,
                         })
