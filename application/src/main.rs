3c08d68b60daccefbfefb4a5137ceca0afa4639a
Robert Jansen <me@rjns.dev>
2025-10-15 11:58:20 +0200

    update dependencies

--- application/src/main.rs
+++ application/src/main.rs
@@ -384,7 +384,7 @@ async fn main() {
 
     tracing::info!("starting http/sftp server");
 
-    rustls::crypto::ring::default_provider()
+    rustls::crypto::aws_lc_rs::default_provider()
         .install_default()
         .expect("Failed to install rustls crypto provider");
 
